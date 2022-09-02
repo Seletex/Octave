45bc1386df626d9f580ec4c3eb430a89b586d1f8
Seletex <sarr1998@hotmail.com>
2022-09-01 22:22:51 -0500

    Create Circuito.m

---
+++ Minimos cuadrados aplicados codigo/Circuito.m
@@ -0,0 +1,15 @@
+clear all
+close all
+clc
+R = 1000;
+C=0.1*10^-6;
+deltaT=0.00001;
+f=(R*C)/deltaT
+Vc(1)=0;
+
+tama=50;
+Vin=ones(tama,2);
+for (i=1:tama-1)
+  Vc(i+1)=(Vin(i) + f*Vc(i) - Vc(i))/f;
+endfor
+plot(Vc);
